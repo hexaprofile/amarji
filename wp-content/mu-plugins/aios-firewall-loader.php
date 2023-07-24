f8940b94983c7767bc6835e072fc9dc5dd6e8a9a
Haider Al Zubaidy <haidercis@gmail.com>
2023-07-24 19:55:01 +0300

    full commit

---
+++ wp-content/mu-plugins/aios-firewall-loader.php
@@ -0,0 +1,6 @@
+<?php
+// Begin AIOWPSEC Firewall
+if (file_exists('/var/www/amarjilibral.org/public_html/aios-bootstrap.php')) {
+	include_once('/var/www/amarjilibral.org/public_html/aios-bootstrap.php');
+}
+// End AIOWPSEC Firewall
